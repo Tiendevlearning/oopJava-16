02ca650d0472dae899165b3654069a6a0f5d0c23
TienLearnCode <103649604+TienLeanCode@users.noreply.github.com>
2023-03-07 22:01:12 +0700

    baithuchanh2402

---
+++ BaiThucHanh/BaiThucHanh1702/Slide35.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Slide35{
+     public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	System.out.print("So no nhat trong hai so la: " + Math.min(a, b));
+  }
+}
