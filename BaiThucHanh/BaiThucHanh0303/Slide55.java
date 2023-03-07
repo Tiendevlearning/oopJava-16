27ec9757bd2981b844ddf1fa5dba9d6c79d6a1e7
TienLearnCode <103649604+TienLeanCode@users.noreply.github.com>
2023-03-07 22:13:43 +0700

    0303

---
+++ BaiThucHanh/BaiThucHanh0303/Slide55.java
@@ -0,0 +1,9 @@
+public class Slide55 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Cac so chia het cho 5 tu 0 đen 200 la:");
+        for (int i = 0; i <= 200; i++) {
+            if (i % 5 == 0)
+                System.out.print(i + " ");
+        }
+    }
+}
