9ace080144aed437a31df3b1417aed2a61f23913
TienLearnCode <103649604+TienLeanCode@users.noreply.github.com>
2023-03-11 15:01:17 +0700

    Delete BaiThucHanh directory

--- BaiThucHanh/BaiThucHanh0303/Slide55.java
+++
@@ -1,9 +0,0 @@
-public class Slide55 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Cac so chia het cho 5 tu 0 đen 200 la:");
-        for (int i = 0; i <= 200; i++) {
-            if (i % 5 == 0)
-                System.out.print(i + " ");
-        }
-    }
-}
